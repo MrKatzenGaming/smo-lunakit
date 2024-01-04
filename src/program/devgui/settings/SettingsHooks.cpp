2bac54fc0a517783a1fc500b082171d8883526ca
MrKatzenGaming <max@langenhan2.de>
2024-01-04 19:10:27 +0100

    revert some changes

--- src/program/devgui/settings/SettingsHooks.cpp
+++ src/program/devgui/settings/SettingsHooks.cpp
@@ -22,11 +22,10 @@ namespace patch = exl::patch;
 namespace inst = exl::armv8::inst;
 namespace reg = exl::armv8::reg;
 
-DevGuiSettings* set = DevGuiManager::instance()->getSettings();
-
 HOOK_DEFINE_TRAMPOLINE(ControlHook) {
     static void Callback(StageScene *scene) {
         PlayerActorHakoniwa* player = tryGetPlayerActorHakoniwa(scene);
+        DevGuiSettings* set = DevGuiManager::instance()->getSettings();
 
         if(player && set->getStateByName("Infinite Cap Bounce")) {
             player->mHackCap->mCapActionHistory->clearCapJump();
@@ -55,7 +54,7 @@ HOOK_DEFINE_TRAMPOLINE(ControlHook) {
 HOOK_DEFINE_TRAMPOLINE(NoclipMovementHook) {
     static void Callback(PlayerActorHakoniwa *player) {
         static bool wasNoclipOn = false;
-        bool isNoclip = set->getStateByName("Noclip");
+        bool isNoclip = DevGuiManager::instance()->getSettings()->getStateByName("Noclip");
 
         if (!isNoclip && wasNoclipOn)
             al::onCollide(player);
@@ -112,7 +111,7 @@ HOOK_DEFINE_TRAMPOLINE(NoclipMovementHook) {
 
 HOOK_DEFINE_TRAMPOLINE(SaveHook) {
     static bool Callback(StageScene* scene) {
-        if (set->getStateByName("Autosave"))
+        if (DevGuiManager::instance()->getSettings()->getStateByName("Autosave"))
             return Orig(scene);
 
         return false;
@@ -121,7 +120,7 @@ HOOK_DEFINE_TRAMPOLINE(SaveHook) {
 
 HOOK_DEFINE_TRAMPOLINE(CheckpointWarpHook) {
     static bool Callback(void* thisPtr) {
-        if (set->getStateByName("Always Allow Checkpoints"))
+        if (DevGuiManager::instance()->getSettings()->getStateByName("Always Allow Checkpoints"))
             return true;
 
         return Orig(thisPtr);
@@ -132,12 +131,17 @@ class ShineInfo;
 
 HOOK_DEFINE_TRAMPOLINE(GreyShineRefreshHook) {
     static bool Callback(GameDataHolderWriter writer, ShineInfo const* shineInfo) {
-         return set->getStateByName("Refresh Gray Moons") ? false : Orig(writer, shineInfo);
+         return DevGuiManager::instance()->getSettings()->getStateByName("Refresh Gray Moons")
+          ? false : Orig(writer, shineInfo);
     }
 };
 HOOK_DEFINE_TRAMPOLINE(ShineRefreshHook) {
     static void Callback(GameDataHolderWriter writer, ShineInfo const* shineInfo) {
-        if (!set->getStateByName("Make Moons Re-Collectable")) 
+
+        exl::util::RwPages ShineRefreshText(exl::util::modules::GetTargetOffset(0x01832301), 24);
+            strncpy((char*)ShineRefreshText.GetRw(), "Lunakit", 24);
+
+        if (!DevGuiManager::instance()->getSettings()->getStateByName("Make Moons Re-Collectable")) 
             Orig(writer, shineInfo);
     }
 };
@@ -155,7 +159,7 @@ HOOK_DEFINE_TRAMPOLINE(DisableMoonLockHook) {
 
 HOOK_DEFINE_TRAMPOLINE(ButtonMotionRollHook) {
     static bool Callback(void* thisPtr) {
-        if (set->getStateByName("Button Motion Roll"))
+        if (DevGuiManager::instance()->getSettings()->getStateByName("Button Motion Roll"))
             return true;
 
         return Orig(thisPtr);
@@ -164,7 +168,7 @@ HOOK_DEFINE_TRAMPOLINE(ButtonMotionRollHook) {
 
 HOOK_DEFINE_TRAMPOLINE(NoDamageHook){
     static void Callback(PlayerHitPointData* hitPointData) {
-        if (!set->getStateByName("No Damage"))
+        if (!DevGuiManager::instance()->getSettings()->getStateByName("No Damage"))
             return Orig(hitPointData);
     }
 };
@@ -185,7 +189,4 @@ void exlSetupSettingsHooks()
     ShineRefreshHook::InstallAtSymbol("_ZN16GameDataFunction11setGotShineE20GameDataHolderWriterPK9ShineInfo");
     ButtonMotionRollHook::InstallAtSymbol("_ZNK23PlayerJudgeStartRolling21isTriggerRestartSwingEv");
     NoDamageHook::InstallAtSymbol("_ZN16GameDataFunction12damagePlayerE20GameDataHolderWriter");
-
-    exl::util::RwPages ShineRefreshText(exl::util::modules::GetTargetOffset(0x01832301), 24);
-        strncpy((char*)ShineRefreshText.GetRw(), "Lunakit", 24);
 }
